86c3d87cd8e53a8b4ae00d55c566425ee98075a8
Matěj Teplý <81610648+Majkey25@users.noreply.github.com>
2024-10-30 19:23:10 +0100

    docker - running

--- Frontend/app.py
+++ Frontend/app.py
@@ -123,5 +123,5 @@ def mix():
         print(f"Error mixing audio files: {e}")
         return jsonify({'error': 'Error mixing audio'}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
